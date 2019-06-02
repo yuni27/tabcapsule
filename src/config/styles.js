78cd7c529de449eaae86dd6844a8e8e49d1f108b
rofiah <>
2019-06-03 00:57:06 +0700

    try to add qrcode scanner

--- src/config/styles.js
+++ src/config/styles.js
@@ -1,124 +1,133 @@
-import { StyleSheet, Platform, Dimensions } from 'react-native';
-import colors from './colors';
-import fonts from './fonts';
+import { StyleSheet, Platform, Dimensions } from "react-native";
+import colors from "./colors";
+import fonts from "./fonts";
 
-var { height, width } = Dimensions.get('window');
+var { height, width } = Dimensions.get("window");
 export default StyleSheet.create({
-	//fonts
-	Bold_14: {
-		fontFamily: fonts.custom.narrowBold,
-		fontSize: 14
-	},
-	Bold_18: {
-		fontFamily: fonts.custom.narrowBold,
-		fontSize: 18
-	},
-	Bold_24: {
-		fontFamily: fonts.custom.narrowBold,
-		fontSize: 24
-	},
-	Reg_14: {
-		fontFamily: fonts.custom.narrowReg,
-		fontSize: 14
-	},
-	Reg_18: {
-		fontFamily: fonts.custom.narrowReg,
-		fontSize: 18
-	},
-	Reg_24: {
-		fontFamily: fonts.custom.narrowReg,
-		fontSize: 24
-	},
+  //fonts
+  Bold_14: {
+    fontFamily: fonts.custom.narrowBold,
+    fontSize: 14
+  },
+  Bold_18: {
+    fontFamily: fonts.custom.narrowBold,
+    fontSize: 18
+  },
+  Bold_24: {
+    fontFamily: fonts.custom.narrowBold,
+    fontSize: 24
+  },
+  Reg_14: {
+    fontFamily: fonts.custom.narrowReg,
+    fontSize: 14
+  },
+  Reg_18: {
+    fontFamily: fonts.custom.narrowReg,
+    fontSize: 18
+  },
+  Reg_24: {
+    fontFamily: fonts.custom.narrowReg,
+    fontSize: 24
+  },
 
-	//Component
-	headerContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		height: 50,
-		paddingRight: 20,
-		paddingLeft: 20,
-		marginTop: Platform.OS === 'android' ? 0 : 20,
-		backgroundColor: colors.primary,
-		alignItems: 'center'
-	},
-	logoHome: {
-		height: 225,
-		borderWidth: 5,
-		borderColor: colors.primary,
-		alignItems: 'center',
-		justifyContent: 'center',
-		width: width - 40,
-		margin: 20
-	},
-	buttonSquare: {
-		height: 50,
-		backgroundColor: colors.primary,
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	slide: {
-		width: width,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#9DD6EB'
-	},
-	wrapper: {
-		backgroundColor: colors.primary
-	},
-	list: {
-		height: 100,
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		marginBottom: 20,
-		alignItems: 'center',
-		borderBottomColor: colors.primary,
-		borderBottomWidth: 1
-	},
-	grid: {
-		justifyContent: 'flex-start',
-		flexDirection: 'row',
-		flexWrap: 'wrap'
-		// flex: 1,
-	},
-	listContainer: {
-		width: '100%'
-	},
-	modal: {
-		width: width / 2,
-		marginLeft: width / 2,
-		overflow: 'hidden',
-		alignItems: 'center',
-		justifyContent: 'flex-start',
-		marginTop: -height * 2 / 3,
-		borderTopLeftRadius: 10,
-		borderBottomLeftRadius: 10
-	},
-	rowModal: {
-		backgroundColor: 'transparent',
-		borderTopWidth: 1,
-		borderTopColor: colors.primary,
-		height: 50,
-		justifyContent: 'center',
-		alignItems: 'center',
-		width: width / 2
-	},
-	tableVoucher: {
-		flexDirection: 'column',
-		alignItems: 'center',
-		justifyContent: 'center',
-		borderColor: colors.primary1,
-		borderWidth: 1,
-		padding: 16
-	},
+  //Component
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: 50,
+    paddingRight: 20,
+    paddingLeft: 20,
+    marginTop: Platform.OS === "android" ? 0 : 20,
+    backgroundColor: colors.primary,
+    alignItems: "center"
+  },
+  logoHome: {
+    height: 225,
+    borderWidth: 5,
+    borderColor: colors.primary,
+    alignItems: "center",
+    justifyContent: "center",
+    width: width - 40,
+    margin: 20
+  },
+  buttonSquare: {
+    height: 50,
+    backgroundColor: colors.primary,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  slide: {
+    width: width,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#9DD6EB"
+  },
+  wrapper: {
+    backgroundColor: colors.primary
+  },
+  list: {
+    height: 100,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    alignItems: "center",
+    borderBottomColor: colors.primary,
+    borderBottomWidth: 1
+  },
+  grid: {
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    flexWrap: "wrap"
+    // flex: 1,
+  },
+  listContainer: {
+    width: "100%"
+  },
+  modal: {
+    width: width / 2,
+    marginLeft: width / 2,
+    overflow: "hidden",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginTop: (-height * 2) / 3,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10
+  },
+  rowModal: {
+    backgroundColor: "transparent",
+    borderTopWidth: 1,
+    borderTopColor: colors.primary,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    width: width / 2
+  },
+  tableVoucher: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: colors.primary1,
+    borderWidth: 1,
+    padding: 16
+  },
 
-	//size
-	sizeLogo: {
-		width: 1182 / 5,
-		height: 553 / 5
-	},
-	sizeIcon: {
-		width: 30,
-		height: 30,
-		tintColor: colors.primary
-	}
+  containerQrcodeScan: {
+    height: height,
+    backgroundColor: colors.darkGrey
+  },
+  contentQrcode: {
+    justifyContent: "flex-start",
+    alignItems: "flex-start"
+  },
+
+  //size
+  sizeLogo: {
+    width: 1182 / 5,
+    height: 553 / 5
+  },
+  sizeIcon: {
+    width: 30,
+    height: 30,
+    tintColor: colors.primary
+  }
 });
